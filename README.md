554b2bae41db114bcb8f13ec5837efe3b3580da5
Subrat <subratpathak95@gmail.com>
2025-02-17 15:55:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Netflix GPT
+
+- Crate React App
+- Configured TailwindCSS into our app
---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      Namaste Everyone, let's build netflix-gpt
+    
+    </div>
+  );
+}
+
+export default App;
