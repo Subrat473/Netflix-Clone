b7126f153950d5c390964b3152322371d329e499
Subrat <85737468+Subrat473@users.noreply.github.com>
2025-03-16 18:01:31 +0000

    Changes

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -4,21 +4,19 @@ import MovieList from "./MovieList";
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
 
+  if (!movies) return null; // Prevent errors if movies are not available
+
   return (
-    movies.nowPlayingMovies && (
-      <div className="bg-black">
-        <div className=" mt-0 md:-mt-52 pl-4 md:pl-12 relative z-20">
-          <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
-          <MovieList title={"Popular"} movies={movies.popularMovies} />
-          <MovieList
-            title={"Upcoming Movies"}
-            movies={movies.nowPlayingMovies}
-          />
-          <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
-        </div>
+    <div className="bg-black">
+      <div className="mt-0 md:-mt-52 pl-4 md:pl-12 relative z-20">
+        <MovieList title="Now Playing" movies={movies.nowPlayingMovies} />
+        <MovieList title="Trending" movies={movies.trendingMovies || []} />
+        <MovieList title="Popular" movies={movies.popularMovies} />
+        <MovieList title="Upcoming Movies" movies={movies.upcomingMovies || []} />
+        <MovieList title="Horror" movies={movies.horrorMovies || []} />
       </div>
-    )
+    </div>
   );
 };
+
 export default SecondaryContainer;
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -2,23 +2,22 @@ import { useSelector } from "react-redux";
 import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
+  useMovieTrailer(movieId); // Ensure this only runs when movieId changes (handled inside hook)
+
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
 
-  useMovieTrailer(movieId);
+  if (!trailerVideo?.key) return null; // Prevent rendering if no trailer is found
 
   return (
-    <div className=" w-screen">
+    <div className="w-screen overflow-hidden">
       <iframe
-        className="w-screen aspect-video"
-        src={
-          "https://www.youtube.com/embed/" +
-          trailerVideo?.key +
-          "?&autoplay=1&mute=1"
-        }
+        className="w-screen aspect-video border-none"
+        src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
     </div>
   );
 };
+
 export default VideoBackground;
